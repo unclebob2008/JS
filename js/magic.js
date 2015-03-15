1819356343f25bad88e6ebc30ae363d01a651a31
SlavaTem <slavatem@rambler.ru>
2015-03-15 09:07:33 +0300

    jqury namespacing

--- js/magic.js
+++ js/magic.js
@@ -1,12 +1,31 @@
-"use strict";
-function clickImg() {
-    var imgUrl = document.getElementById("bfImg").src;
-    if (imgUrl.indexOf("smile3.jpg") > 0) {
-//      console.log(imgUrl.indexOf("balls.gif"));
-//      alert("Рад вас видеть, " + userName + "." + " Нажми ОК и я превращусь в бабочку");
-        document.getElementById("bfImg").src = "images/smile1.gif";
-        document.getElementById("head01").innerHTML = "Хи-Хи-Хи";
-        setTimeout("document.getElementById('bfImg').src = 'images/smile3.jpg'; \
-        document.getElementById('head01').innerHTML = 'Привет. Кликни по мне мышкой.';", 1000);
+(function(sgames, $, undefined) {
+    "use strict";
+    sgames.clickImg = function() {
+        var imgUrl = $("#bfImg").attr('src');
+        if (imgUrl.indexOf("smile3.jpg") > 0) {
+            $("#bfImg").attr('src', "images/smile1.gif");
+            $("#head01").text("Хи-Хи-Хи");
+            setTimeout("$('#bfImg').attr('src', 'images/smile3.jpg'); \
+            $('#head01').text('Привет. Кликни по мне мышкой.')", 1000);
+        }
+    };
+    sgames.setMinesP1 = function() {
+        $("#bfImg").remove();
+        $("#head01").remove();
+        $("#div01").css("text-align", "left");
+        var p1 = '<h1 id="head01">Сапёр</h1> \
+        Ширина поля: <input id="wF" type="number" name="widthField" min="3" max="30" value=10> \
+        <br> \
+        Высота поля: <input id="hF" type="number" name="heightField" min="3" max="30" value=10> \
+        <br> \
+        Количество мин: <input id="pM" type="number" name="procMines" min="10" max="50" value=25>% от размера поля \
+        <br> \
+        <input type="button" name="startBut" onclick="sgames.setTable();" value="Начать новую игру"> \
+        <br/> \
+        <br/> \
+        <h2 id="baner"></h2>'
+        $("#div01").append(p1)
     }
-}
+}(window.sgames = window.sgames || {}, jQuery));
+
+
--- js/matrix.js
+++ js/matrix.js
@@ -1,170 +1,172 @@
 "use strict";
 
-var widthField, heightField, procMines, numMines;
-var arrField = {};
+sgames.widthField;
+sgames.heightField;
+sgames.procMines;
+sgames.numMines;
+sgames.arrField = {};
 
-function setTable() {
-  widthField = document.getElementById("wF").value;
-  heightField = document.getElementById("hF").value;
-  procMines = document.getElementById("pM").value;
-  numMines = widthField * heightField / 100 * procMines;
-  document.getElementById("baner").innerHTML = "";
-  if (widthField >= 3 && widthField <= 30 && heightField >= 3 && heightField <= 30 && procMines >= 10 && procMines <= 50) {
-    var fld = document.getElementById('field');
-    var body = document.body;
+sgames.setTable = function () {
+  sgames.widthField = $("#wF").val();
+  sgames.heightField = $("#hF").val();
+  sgames.procMines = $("#pM").val();
+  sgames.numMines = sgames.widthField * sgames.heightField / 100 * sgames.procMines;
+  $("#baner").text("");
+  if (sgames.widthField >= 3 && sgames.widthField <= 30 && sgames.heightField >= 3 &&
+      sgames.heightField <= 30 && sgames.procMines >= 10 && sgames.procMines <= 50) {
     var tbl  = document.createElement('table');
     tbl.id = 'field';
-    for(var i = 0; i < heightField; i++) {
+    for(var i = 0; i < sgames.heightField; i++) {
         var tr = tbl.insertRow();
-        for(var j = 0; j < widthField; j++) {
+        for(var j = 0; j < sgames.widthField; j++) {
             var td = tr.insertCell();
             td.appendChild(document.createTextNode(''));
-            td.id = i + "." + j;
-            td.onclick = function(){clickCell();};
-            arrField[td.id] = 0;
+            td.id = i + "_" + j;
+            td.onclick = function(){sgames.clickCell();};
+            sgames.arrField[td.id] = 0;
         }
     }
-    if (fld) {
-        body.replaceChild(tbl, fld);
+    if ($("#field").length) {
+        $("#field").replaceWith(tbl);
     } else {
-        body.appendChild(tbl);
+        $("body").append(tbl);
     }
-    for (var m = 0; m < numMines; m++) {
-        var y = Math.floor((Math.random() * heightField));
-        var x = Math.floor((Math.random() * widthField));
-        if (arrField[y + "." + x] !== 9) {
-            arrField[y + "." + x] = 9;
+    for (var m = 0; m < sgames.numMines; m++) {
+        var y = Math.floor((Math.random() * sgames.heightField));
+        var x = Math.floor((Math.random() * sgames.widthField));
+        if (sgames.arrField[y + "_" + x] !== 9) {
+            sgames.arrField[y + "_" + x] = 9;
         } else {
             m--;
         }
     }
-    for(var i = 0; i < heightField; i++) {
-        for(var j = 0; j < widthField; j++) {
-            if (arrField[i + "." + j] == 9) {
-                var nb = collectNear(i, j);
+    for(var i = 0; i < sgames.heightField; i++) {
+        for(var j = 0; j < sgames.widthField; j++) {
+            if (sgames.arrField[i + "_" + j] == 9) {
+                var nb = sgames.collectNear(i, j);
                 for (var k = 0; k < nb.length; k++) {
-                    if (arrField[nb[k]] != 9) {
-                        arrField[nb[k]]++;
+                    if (sgames.arrField[nb[k]] != 9) {
+                        sgames.arrField[nb[k]]++;
                     }
                 }
             }
         }
     }
   } else {
-      document.getElementById("baner").innerHTML = "Ширина и высота могут быть от 3 до 30, количество мин от 10 до 50%.";
+      $("#baner").text("Ширина и высота могут быть от 3 до 30, количество мин от 10 до 50%.");
   }
-}
+};
 
-function clickCell() {
+sgames.clickCell = function() {
     var clickId = event.target.id;
     if (event.button == 0) {
-        if (arrField[clickId] !== 9) {
-            document.getElementById(clickId).style.background = "white";
-            if (arrField[clickId] !== 0) {
-                document.getElementById(clickId).childNodes[0].nodeValue = arrField[clickId];
+        if (sgames.arrField[clickId] !== 9) {
+            $("#" + clickId).css("background", "white");
+            if (sgames.arrField[clickId] !== 0) {
+                $("#" + clickId).text(sgames.arrField[clickId]);
             } else {
-                openNear(clickId);
+                sgames.openNear(clickId);
             }
-            checkEndGame();
+            sgames.checkEndGame();
         } else {
-            showMines();
-            document.getElementById(clickId).style.background = "red";
-            document.getElementById("baner").innerHTML = "Вы проиграли. Попробуйте ещё раз.";    
+            sgames.showMines();
+            $("#" + clickId).css("background", "red");
+            $("#baner").text("Вы проиграли. Попробуйте ещё раз.");    
         }
     } else {
-        var checkBG = document.getElementById(clickId).style.background;
+        var checkBG = $("#" + clickId).css("background");
         switch (checkBG) {
             case '':
-                document.getElementById(clickId).style.background = "blue";
+                $("#" + clickId).css("background", "blue");
                 break;
             case 'blue':
-                document.getElementById(clickId).attributes.removeNamedItem("style");
+                $("#" + clickId).removeAttr("style");
                 break;
         }
-        checkEndGame();
+        sgames.checkEndGame();
     }
-}
+};
 
-function checkEndGame() {
+sgames.checkEndGame = function() {
     var count = 0;
     var mines = 0;
-    for (var i = 0; i < heightField; i++) {
-        for (var j = 0; j < widthField; j++) {
-            var checkBG = document.getElementById(i + "." + j).style.background;
-            if (arrField[i + "." + j] !== 9 && checkBG == '') count++;
-            if (arrField[i + "." + j] == 9 && checkBG == 'blue') mines++;
+    for (var i = 0; i < sgames.heightField; i++) {
+        for (var j = 0; j < sgames.widthField; j++) {
+            var checkBG = $("#" + i + "_" + j).css("background");
+            console.log(checkBG);
+            if (sgames.arrField[i + "_" + j] !== 9 && checkBG == '') count++;
+            if (sgames.arrField[i + "_" + j] == 9 && checkBG == 'blue') mines++;
         }
     }
-    if (count == 0 || mines == numMines) {
-        document.getElementById("baner").innerHTML = "Вы победили. Ура!!!";
-        showMines();
+    if (count == 0 || mines == sgames.numMines) {
+        $("#baner").text("Вы победили. Ура!!!");
+        sgames.showMines();
     }
-}
+};
 
-function openNear(clId) {
-    var id = clId.split(".");
+sgames.openNear = function(clId) {
+    var id = clId.split("_");
     var i = Number(id[0]);
     var j = Number(id[1]);
-    var idCol = collectNear(i, j);
+    var idCol = sgames.collectNear(i, j);
     for (var k = 0; k < idCol.length; k++) {
-        var checkBG = document.getElementById(idCol[k]).style.background;
+        var checkBG = $("#" + idCol[k]).css("background");
         if (checkBG !== 'white') {
-            document.getElementById(idCol[k]).style.background = "white";
-            if (arrField[idCol[k]] !== 0) {
-                document.getElementById(idCol[k]).childNodes[0].nodeValue = arrField[idCol[k]];
+            $("#" + idCol[k]).css("background", "white");
+            if (sgames.arrField[idCol[k]] !== 0) {
+                $("#" + idCol[k]).text(sgames.arrField[idCol[k]]);
             } else {
-                openNear(idCol[k]);
+                sgames.openNear(idCol[k]);
             }
         }
     }
-}
+};
 
-function showMines() {
-    for (var i = 0; i < heightField; i++) {
-         for (var j = 0; j < widthField; j++) {
-              if (arrField[i + "." + j] == 9) {
-                        document.getElementById(i + "." + j).childNodes[0].nodeValue =
-                        'Bomb';
-                        document.getElementById(i + "." + j).style.color = "white";
-                        document.getElementById(i + "." + j).style.fontSize = "10px";
+sgames.showMines = function() {
+    for (var i = 0; i < sgames.heightField; i++) {
+         for (var j = 0; j < sgames.widthField; j++) {
+              if (sgames.arrField[i + "_" + j] == 9) {
+                        $("#" + i + "_" + j).text('Bomb');
+                        $("#" + i + "_" + j).css("color", "white");
+                        $("#" + i + "_" + j).css("font-size", "10px");
               }
           }
     }
-}
+};
 
-function collectNear(x, y) {
+sgames.collectNear = function(x, y) {
     var idCol = [];
     var n = 0;
     if (y - 1 >= 0) {
-        idCol[n] = x + "." + (y - 1);
+        idCol[n] = x + "_" + (y - 1);
         n++;
     }
-    if (y + 1 < widthField) {
-        idCol[n] = x + "." + (y + 1);
+    if (y + 1 < sgames.widthField) {
+        idCol[n] = x + "_" + (y + 1);
         n++;
     }
     if (x - 1 >= 0) {
-        idCol[n] = (x - 1) + "." + y;
+        idCol[n] = (x - 1) + "_" + y;
         n++;
     }
-    if (x + 1 < heightField) {
-        idCol[n] = (x + 1) + "." + y;
+    if (x + 1 < sgames.heightField) {
+        idCol[n] = (x + 1) + "_" + y;
         n++;
     }
     if (y - 1 >= 0 && x - 1 >= 0) {
-        idCol[n] = (x - 1) + "." + (y - 1);
+        idCol[n] = (x - 1) + "_" + (y - 1);
         n++;
     }
-    if (y - 1 >= 0 && x + 1 < heightField) {
-        idCol[n] = (x + 1) + "." + (y - 1);
+    if (y - 1 >= 0 && x + 1 < sgames.heightField) {
+        idCol[n] = (x + 1) + "_" + (y - 1);
         n++;
     }
-    if (x - 1 >= 0 && y + 1 < widthField) {
-        idCol[n] = (x - 1) + "." + (y + 1);
+    if (x - 1 >= 0 && y + 1 < sgames.widthField) {
+        idCol[n] = (x - 1) + "_" + (y + 1);
         n++;
     }
-    if (x + 1 < heightField && y + 1 < widthField) {
-        idCol[n] = (x + 1) + "." + (y + 1);
+    if (x + 1 < sgames.heightField && y + 1 < sgames.widthField) {
+        idCol[n] = (x + 1) + "_" + (y + 1);
         n++;
     }
     return idCol;
