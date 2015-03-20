04caae3adfc75d083c5d74acaf40518a5cfc4310
SlavaTem <slavatem@rambler.ru>
2015-03-20 22:06:03 +0300

    tetris left-right-color

--- js/tetris.js
+++ js/tetris.js
@@ -1,7 +1,7 @@
 "use strict";
 
 _sgames.Tetris = function() {
-    var self = this;
+    
     var bars = [];
     bars[0] = ["0_4","0_5","1_4","1_5"];
     bars[1] = ["0_3","0_4","0_5","0_6"];
@@ -19,66 +19,122 @@ _sgames.Tetris = function() {
     bars[13] = ["0_4","0_5","1_5","1_6"];
     bars[14] = ["1_4","1_5","0_5","0_6"];
     
+    var blockColors = ["red", "orange", "yellow", "green", "blue", "violet"];
+    
     var yPos = [];
     var xPos = [];
+    var table = {};
+    var delay = 0;
+    var timer;
+    var colCur = "";
     
     this.setGlass = function() {
         $("#div01").empty();
-        var tetrBut = '<button onclick="tetris.startGame();">Start game</button>';
+        var butStart = '<button id="butStart" onclick="tetris.startGame();">Start game</button>';
+        var butPause = '<button id="butPause" onclick="tetris.pauseGame();">Pause game</button>';
         var top = 50;
         var glass  = document.createElement('div');
         glass.id = "div03";
-        for(var i = 0; i < 20; i++) {
+        for (var i = 0; i < 20; i++) {
             var left = 100;
-            for(var j = 0; j < 10; j++) {
+            for (var j = 0; j < 10; j++) {
                 var block = document.createElement('div');
                 block.style.top = top + "px";
                 block.style.left = left +"px";
-                block.className = "block blocknow";
+                block.className = "block";
                 block.id = i + "_" + j;
+                table[block.id] = 0;
                 glass.appendChild(block);
                 left += 30
             }
             top += 26
         }
-        $("#div01").append(tetrBut, glass);
+        $("#div01").append(butStart, butPause, glass);
+        pressedKey();
     };
     
-    this.startGame = function() {
+    var getNewBlock = function() {
         var barCur = bars[Math.floor((Math.random() * 14))];
+        colCur = blockColors[Math.floor((Math.random() * 5))];
+        console.log(colCur);
         for (var i = 0; i < 4; i++) {
             var id = barCur[i].split("_");
             yPos[i] = Number(id[0]);
             xPos[i] = Number(id[1]);
-            $("#" + yPos[i] + "_" + xPos[i]).removeClass("block").addClass("blocknow");
+            $("#" + barCur[i]).css("background-color", colCur);
+            table[barCur[i]] = 1;
         }
-        pressedKey();
-        moveBlock(yPos, xPos, "y", 19, 1, 1000);
     };
     
-    var moveBlock = function(yCoord, xCoord, dir, gap, step, time) { 
-        var coord = (dir == "y") ? yCoord : xCoord;
-        var delta = coord[0] + gap;
-        var timer = setInterval(drawBlock, time);
-        function drawBlock() {
-            for (var i = 0; i < 4; i++) {
-                $("#" + yPos[i] + "_" + xPos[i]).removeClass("blocknow").addClass("block");
-                coord[i] += step;
-            }
-            for (var i = 0; i < 4; i++) {
-                $("#" + yPos[i] + "_" + xPos[i]).removeClass("block").addClass("blocknow");
-            }
-            if (coord[3] == delta) {
+    this.pauseGame = function() {
+        $("#butPause").attr("onclick", "tetris.resumeGame();").text("Resume game");
+        clearInterval(timer);
+    };
+    
+    this.resumeGame = function() {
+        $("#butPause").attr("onclick", "tetris.pauseGame();").text("Pause game");
+        fallingBlock();
+    };
+    
+    this.startGame = function() {
+        getNewBlock();
+        delay = 1000;
+        fallingBlock();
+    };
+    
+    var fallingBlock = function() { 
+        timer = setInterval(fallBlock, delay);
+        function fallBlock() {
+            drawBlock(3);
+            var yMax = Math.max.apply(Math, yPos);
+            if (yMax == 19) {
                 clearInterval(timer);
+                tetris.startGame();
             }
         }
+    };
+    
+    var moveLeft = function() {
+        var xMin = Math.min.apply(Math, xPos);
+        if ((xMin - 1) >= 0) {
+            drawBlock(2);
+        }
+    };
+    
+    var moveRight = function() {
+        var xMax = Math.max.apply(Math, xPos);
+        if ((xMax + 1) < 10) {
+            drawBlock(1);
+        }
+    };
+    
+    var drawBlock = function(dir) {
+        for (var i = 0; i < 4; i++) {
+             $("#" + yPos[i] + "_" + xPos[i]).css("background-color", "");
+             table[yPos[i] + "_" + xPos[i]] = 0;
+        }
+        for (var i = 0; i < 4; i++) {
+            switch (dir) {
+                case 1:
+                    xPos[i]++;
+                    break;
+                case 2:
+                    xPos[i]--;
+                    break;
+                case 3:
+                    yPos[i]++;
+                    break;
+            }
+            $("#" + yPos[i] + "_" + xPos[i]).css("background-color", colCur);
+            table[yPos[i] + "_" + xPos[i]] = 1;
+         }
     };
     
     var pressedKey = function() {
         window.onkeydown = function(pKey) {
-            if(pKey.which == 65) moveBlock(yPos, xPos, "x", 1, -1, 1000);
+            if(pKey.which == 65) moveLeft();
             if(pKey.which == 87) console.log('rotate');
-            if(pKey.which == 68) moveBlock(yPos, xPos, "x", 1, 1, 1000);
+            if(pKey.which == 68) moveRight();
             if(pKey.which == 83) console.log('drop');
         };
     }
